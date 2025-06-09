ed24229f62dde151f81768fb42a9956f79499087
Vatsal <vatsalnpatel1@gmail.com>
2025-06-09 20:00:22 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-from MLproject.utils import logger
-logger.info("Hello World")
+from MLproject import logger
+from MLproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
