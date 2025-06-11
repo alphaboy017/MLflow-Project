73f8cb4f19355511bba6ad16bcea7cc0d5fafceb
Vatsal <vatsalnpatel1@gmail.com>
2025-06-11 23:30:16 +0530

    Data Transformation and Model Traniner Added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from MLproject import logger
 from MLproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from MLproject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from MLproject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from MLproject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -23,3 +25,25 @@ except Exception as e:
         raise e
 
 
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
